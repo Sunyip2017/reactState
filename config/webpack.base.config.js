96ff9ec7a1efb22c443d33292f56733f27ab37ff
sunyipeng <18364280936@163.com>
2022-03-22 10:56:11 +0800

    feat:新增eslint配置和ts配置

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -7,13 +7,17 @@ const webpackBaseConfig = {
         filename:'[name].[fullhase:4].js'
     },
     resolve: {
-        extensions: ['.js','.jsx']
+        extensions: ['.js','.jsx','.tsx']
     },
     module: {
         rules: [
             {
                 test:/\.js[x]/,
                 use:'babel-loader'
+            },
+            {
+                test:/\.ts[x]/,
+                use:'ts-loader'
             },
             {
                 test:/\.(sc|c)ss/,
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
-class App extends Component {
+
+class App extends Component{
     render () {
         return (
             <div>Hello World!</div>
